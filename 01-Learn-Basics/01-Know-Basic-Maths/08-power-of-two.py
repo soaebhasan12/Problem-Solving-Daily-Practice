bce32f0e116cdc4f79625a207cd26c2fa5482c0c
unknown <saibuhasan22@gmail.com>
2026-02-26 23:46:46 +0530

    Power of Two

---
+++ 01-Learn-Basics/01-Know-Basic-Maths/08-power-of-two.py
@@ -0,0 +1,44 @@
+# Power of Two
+
+"""
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+Example 2:
+
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: n = 3
+Output: false
+"""
+
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        
+        # BRUTE FORCE SOLUTION - O(log2(n))
+        
+        if n <= 0:
+            return False
+
+        while n > 1:
+            if n % 2 == 0:
+                n = n // 2
+            else:
+                return False
+        
+        return True
+
+        # OPTIMIZED SOLUTION - O(1)
+        return n > 0 and n & (n - 1) == 0
